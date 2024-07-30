eb4259df125843fc9ffa0796bc4a235b164d97b4
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-30 21:14:14 +0300

    chore: Update App.tsx to separate sections for state, behavior, and rendering

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,19 @@
-import { Button } from "./components/ui/button";
-
 export default function App() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline"> Hello world!  </h1>
-      <Button>Click me</Button>
+    /**
+     * ! STATE (état, données) de l'application
+     */
     
-    </div>
-  )
+    
+    /**
+     * ! COMPORTEMENT (méthodes, fonctions) de l'application
+     */
+    
+    
+    /**
+     * ! AFFICHAGE (render) de l'application
+     */
+    return (
+        <>
+        </>
+    )
 }
