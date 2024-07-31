12281495c9fe94d3c8918fceb8697529627ecd4d
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-31 20:10:26 +0300

    chore: Update Login page component and add LoginForm component

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,24 +1,19 @@
-export default function Login() {
+export default function LoginForm() {
     /**
      * ! STATE (état, données) de l'application
      */
-
-
+    
+    
     /**
      * ! COMPORTEMENT (méthodes, fonctions) de l'application
      */
-
-
+    
+    
     /**
      * ! AFFICHAGE (render) de l'application
      */
     return (
         <>
-            <div className="container min-h-screen">
-                <div className="flex flex-col justify-center items-center">
-
-                </div>
-            </div>
         </>
     )
 }
