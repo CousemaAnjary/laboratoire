81ec7d90b27fc371389654e4ae2f456aa7c00f7f
CousemaAnjary <anjaryabdillah@gmail.com>
2024-07-31 18:58:10 +0300

    chore: Add routing to App component using react-router-dom

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,25 @@
+import { Route, Routes } from "react-router-dom"
+
+
 export default function App() {
     /**
      * ! STATE (état, données) de l'application
      */
-    
-    
+
+
     /**
      * ! COMPORTEMENT (méthodes, fonctions) de l'application
      */
-    
-    
+
+
     /**
      * ! AFFICHAGE (render) de l'application
      */
     return (
         <>
+            <Routes>
+                <Route path="/" element="" />
+            </Routes>
         </>
     )
 }
