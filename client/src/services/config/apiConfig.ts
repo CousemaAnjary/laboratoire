07b188e2d906b17eb23fc0098637682ae10794d3
CousemaAnjary <anjaryabdillah@gmail.com>
2024-08-03 20:00:17 +0300

    chore: Update package.json dependencies for axios and js-cookie

---
+++ client/src/services/config/apiConfig.ts
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import Cookies from 'node_modules/@types/js-cookie'
+
+
+// Récupérer l'URL de base de l'API à partir des variables d'environnement
+const API_URL: string = import.meta.env.VITE_BACKEND_API_URL
+
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+        "Content-Type": "application/json"
+    },
+    withCredentials: true // Pour envoyer les cookies avec les requêtes
+})
+
+// Ajouter un intercepteur de requêtes pour inclure le token dans les en-têtes
+api.interceptors.request.use((config) => {
+
+    // Récupérer le token d'authentification depuis le cookie
+    const token = Cookies.get('auth_token')
+
+    if (token) {
+        // Ajouter le token JWT aux en-têtes d'authentification
+        config.headers.Authorization = `Bearer ${token}`
+    }
+
+    return config // Retourner la configuration modifiée
+
+}, (error) => {
+    return Promise.reject(error)
+})
+
+
+export default api
