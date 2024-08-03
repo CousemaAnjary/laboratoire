85de8e9e4c9d5decfcb9271e62433a12874cb06b
CousemaAnjary <anjaryabdillah@gmail.com>
2024-08-03 19:06:50 +0300

    chore: Update AuthenticatedUserSessionController to store authentication token in a cookie

--- api/app/Http/Controllers/Api/Auth/AuthenticatedUserSessionController.php
+++ api/app/Http/Controllers/Api/Auth/AuthenticatedUserSessionController.php
@@ -27,8 +27,8 @@ class AuthenticatedUserSessionController extends Controller
             return response()->json([
                 'user' => $user,
                 'message' => 'User a été authentifié avec succès.'
-            ], 200)->cookie('auth_token',  $token, 60 * 24, '/', null, true, true); // Nom du cookie, Valeur du cookie (token), Durée de vie (1 jour), Chemin (tout le site), Domaine (domaine actuel), Secure (HTTPS uniquement), HttpOnly (inaccessible via JavaScript)
-            
+            ], 200)->cookie('auth_token',  $token, 60 * 24, '/', null, env('APP_ENV') !== 'local', true); // Nom du cookie, Valeur du cookie (token), Durée de vie (1 jour), Chemin (tout le site), Domaine (domaine actuel), Secure (true pour HTTPS en production, false en local), HttpOnly (inaccessible via JavaScript)
+
         } else {
             // Retourner une réponse JSON avec un message d'erreur
             return response()->json([
