425dda800b848dccf3e58ee718b1eebe6df5bef1
CousemaAnjary <anjaryabdillah@gmail.com>
2024-08-05 19:50:39 +0300

    chore: Ajouter la fonctionnalité de téléchargement d'image lors de l'inscription de l'utilisateur

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -178,7 +178,7 @@ export default function RegisterForm() {
                                 <div className="absolute inset-0 flex items-center">
                                     <span className="w-full border-t"></span>
                                 </div>
-                                <div className="relative flex justify-center text-xs uppercase ">
+                                <div className="relative flex justify-center text-xs uppercase">
                                     <span className="bg-background px-2 text-muted-foreground">Ou continuer avec</span>
                                 </div>
                             </div>
