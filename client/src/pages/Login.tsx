3a9bb69bec4123e407bf5e7bd705069dfdf5b094
CousemaAnjary <anjaryabdillah@gmail.com>
2024-08-06 18:38:00 +0300

    chore: Ajouter la fonctionnalité de téléchargement d'image lors de l'inscription de l'utilisateur

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -7,10 +7,12 @@ export default function Login() {
      * ! STATE (état, données) de l'application
      */
 
+
     /**
      * ! COMPORTEMENT (méthodes, fonctions) de l'application
      */
 
+    
     /**
      * ! AFFICHAGE (render) de l'application
      */
