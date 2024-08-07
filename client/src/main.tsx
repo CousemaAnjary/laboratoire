25e412863f1d382d359bc85c936bab35c9ba423f
CousemaAnjary <anjaryabdillah@gmail.com>
2024-08-07 20:13:16 +0300

    main

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
