96c616e34a54b15980e3e714ce4ba857601b0e2b
CousemaAnjary <anjaryabdillah@gmail.com>
2024-08-11 12:10:19 +0300

    chore: Import index.css in main.tsx for consistent styling

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,8 @@
+import './index.css'
 import React from 'react'
 import App from './App.tsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
-
+import { BrowserRouter } from 'react-router-dom'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
