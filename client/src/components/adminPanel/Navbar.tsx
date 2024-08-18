c566897e48d75fa0d42b39b655d6297735b092ad
CousemaAnjary <anjaryabdillah@gmail.com>
2024-08-18 22:23:29 +0300

    chore: Update Navbar component to include SheetMenu and link to the dashboard

--- client/src/components/adminPanel/Navbar.tsx
+++ client/src/components/adminPanel/Navbar.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom"
 import { Button } from "../ui/button"
 import ModeToggle from "./ModeToggle"
 import UserDropdownMenu from "./UserDropdownMenu"
+import { SheetMenu } from "./SheetMenu"
 
 
 export default function Navbar(): JSX.Element {
@@ -23,7 +24,7 @@ export default function Navbar(): JSX.Element {
             <div className="sticky top-0 z-10 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 dark:shadow-secondary">
                 <div className="mx-4 sm:mx-8 flex h-14 items-center">
                     <div className="flex items-center space-x-4 lg:space-x-0">
-                        {/* <SheetMenu /> */}
+                         <SheetMenu /> 
                         <Link to="/dashboard">
                             <Button variant="ghost">Dashboard</Button>
                         </Link>
