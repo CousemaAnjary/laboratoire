1f66c48a26531ff3fec85719e3c4abe921923840
CousemaAnjary <anjaryabdillah@gmail.com>
2024-08-19 20:16:46 +0300

    chore: Update Dashboard component to improve responsiveness and add sidebar toggle functionality

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -4,28 +4,28 @@ import { cn } from "@/lib/utils";
 import useSidebarToggle from "@/hooks/useSidebarToggle";
 
 export default function Dashboard({ children }: { children: React.ReactNode }) {
-  const { isOpen } = useSidebarToggle();
+    const { isOpen } = useSidebarToggle();
 
-  return (
-    <div className="min-h-screen flex">
-      {/* Sidebar */}
-      <Sidebar />
+    return (
+        <div className="min-h-screen flex">
+            {/* Sidebar */}
+            <Sidebar />
 
-      {/* Contenu principal */}
-      <div
-        className={cn(
-          "flex-grow transition-all duration-300",
-          isOpen ? "ml-72" : "ml-[90px]"
-        )}
-      >
-        {/* Navbar */}
-        <Navbar />
+            {/* Contenu principal */}
+            <div
+                className={cn(
+                    "flex-grow transition-all duration-300 ease-in-out",
+                    isOpen ? "ml-72" : "ml-[90px]"
+                )}
+            >
+                {/* Navbar */}
+                <Navbar />
 
-        {/* Main content */}
-        <main className="p-4">
-          {children}
-        </main>
-      </div>
-    </div>
-  );
+                {/* Main content */}
+                <main className="p-4">
+                    {children}
+                </main>
+            </div>
+        </div>
+    );
 }
