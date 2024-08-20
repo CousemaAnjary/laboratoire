5ce82f16c53b85dfe80d997e5cb7da0db83ace65
CousemaAnjary <anjaryabdillah@gmail.com>
2024-08-20 22:44:09 +0300

    chore: Update useSidebarToggle hook to persist sidebar state in localStorage

--- client/src/hooks/useSidebarToggle.tsx
+++ client/src/hooks/useSidebarToggle.tsx
@@ -2,21 +2,21 @@ import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface useSidebarToggleStore {
-  isOpen: boolean;
-  setIsOpen: () => void;
+    isOpen: boolean;
+    setIsOpen: () => void;
 }
 
 export const useSidebarToggle = create(
-  persist<useSidebarToggleStore>(
-    (set, get) => ({
-      isOpen: true,
-      setIsOpen: () => {
-        set({ isOpen: !get().isOpen });
-      }
-    }),
-    {
-      name: 'sidebarOpen', // Nom de l'état stocké dans le localStorage
-      storage: createJSONStorage(() => localStorage) // Utilisation du localStorage pour persister l'état
-    }
-  )
+    persist<useSidebarToggleStore>(
+        (set, get) => ({
+            isOpen: true,
+            setIsOpen: () => {
+                set({ isOpen: !get().isOpen });
+            }
+        }),
+        {
+            name: 'sidebarOpen', // Nom de l'état stocké dans le localStorage
+            storage: createJSONStorage(() => localStorage) // Utilisation du localStorage pour persister l'état
+        }
+    )
 );
