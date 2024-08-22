32ac03f2e34b4ba15cc7e24fc7a2a53ff4e0be2a
CousemaAnjary <anjaryabdillah@gmail.com>
2024-08-22 20:48:18 +0300

    chore: Update npm dependencies to include @lordicon/element and lottie-web

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,14 @@
 import './index.css'
 import React from 'react'
 import App from './App.tsx'
+import lottie from 'lottie-web'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { defineElement } from '@lordicon/element'
+
+// Définir l'élément "lord-icon" pour l'animation
+defineElement(lottie.loadAnimation)
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
