10ca203c34d7292dab7f43d4cbca735f730ffb36
CousemaAnjary <anjaryabdillah@gmail.com>
2024-08-26 23:36:56 +0300

    chore: Update SkeletonList component to use wider Skeleton for loading state

--- client/src/components/loading/SkeletonList.tsx
+++ client/src/components/loading/SkeletonList.tsx
@@ -17,10 +17,10 @@ export default function SkeletonList() {
     return (
         <>
             <div className="flex flex-col space-y-3">
-                <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+                <Skeleton className="h-[125px] w-[280px] rounded-xl" />
                 <div className="space-y-2">
                     <Skeleton className="h-4 w-[250px]" />
-                    <Skeleton className="h-4 w-[200px]" />
+                    {/* <Skeleton className="h-4 w-[200px]" /> */}
                 </div>
             </div>
         </>
