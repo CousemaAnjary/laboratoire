0d84ba3bd81064b3f61187b08eb347f9421d78c0
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-20 20:35:25 +0300

    Ajouter les pages de connexion et d'inscription pour l'authentification

---
+++ client/src/modules/auth/pages/Login.tsx
@@ -0,0 +1,19 @@
+export default function Login() {
+    /**
+     * ! STATE (état, données) de l'application
+     */
+    
+    
+    /**
+     * ! COMPORTEMENT (méthodes, fonctions) de l'application
+     */
+    
+    
+    /**
+     * ! AFFICHAGE (render) de l'application
+     */
+    return (
+        <>
+        </>
+    )
+}
---
+++ client/src/modules/auth/services/authService.ts
@@ -0,0 +1,26 @@
+import api from "@/services/apiConfig"
+
+
+// Inscription d'un utilisateur
+export const register = async () => {
+    try {
+        // Appel à l'API pour enregistrer un nouvel utilisateur
+        const response = await api.post('/register')
+        return response.data // Retourner les données de la réponse de l'API
+
+    } catch (error) {
+        console.error('Erreur lors de l\'inscription:', error)
+    }
+}
+
+// Connexion d'un utilisateur
+export const login = async () => {
+    try {
+        // Appel à l'API pour authentifier un utilisateur
+        const response = await api.post('/login')
+        return response.data // Retourner les données de la réponse de l'API
+
+    } catch (error) {
+        console.error('Erreur lors de l\'authentification:', error)
+    }
+}
