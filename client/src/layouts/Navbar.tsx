e45f6d8e6d0f26b745370e099a60310732a189b5
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-22 23:32:07 +0300

    Refactor import paths in Navbar component and add RotateTextButton component

--- client/src/layouts/Navbar.tsx
+++ client/src/layouts/Navbar.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/core/components/ui/button"
+import RotateTextButton from "./ui/RotateTextButton"
+
 
 export default function Navbar() {
     /**
@@ -17,7 +20,9 @@ export default function Navbar() {
         <>
             <nav className="container-fluid h-16 border-b flex justify-between items-center">
                 <div className="logo ms-20">
-                    <h1>LOGO</h1>
+                     {/* <h1>LOGO</h1>  */}
+                     <RotateTextButton text="Hover me" />
+                     <Button>Hover me</Button>
                 </div>
 
                 <div className="navbar ms-40">
---
+++ client/src/layouts/ui/RotateTextButton.tsx
@@ -0,0 +1,52 @@
+
+// Type d'un composant bouton qui fait tourner son texte à chaque hover
+type RotateTextButtonProps = {
+    text: string
+    onClick?: () => void
+    className?: string
+    size?: "default" | "sm" | "lg" | "icon";
+}
+
+// Définit les classes pour chaque taille
+const sizeClasses = {
+    default: "px-4 py-2",
+    sm: "h-8 rounded-md px-3 text-xs",
+    lg: "h-10 rounded-md px-8",
+    icon: "h-9 w-9",
+}
+
+export default function RotateTextButton({ text, size = "default", onClick, className = '' }: RotateTextButtonProps) {
+    /**
+     * ! STATE (état, données) de l'application
+     */
+    // Découper le texte en tableau de caractères
+    const chars = text.split('')
+
+    /**
+     * ! COMPORTEMENT (méthodes, fonctions) de l'application
+     */
+
+
+    /**
+     * ! AFFICHAGE (render) de l'application
+     */
+    return (
+        <>
+            <button
+                onClick={onClick}
+                className={`group relative overflow-hidden border rounded-md text-sm font-medium bg-transparent ${sizeClasses[size]} ${className}`}
+            >
+                <span className="relative inline-flex">
+                    {chars.map((char, index) => (
+                        <span
+                            key={index}
+                            className={`duration-700 [transition-delay:${index * 0.02}s] group-hover:[transform:rotateY(360deg)]`}
+                        >
+                            {char === ' ' ? '\u00A0' : char} {/* Gérer l'espace comme un espace insécable */}
+                        </span>
+                    ))}
+                </span>
+            </button>
+        </>
+    )
+}
