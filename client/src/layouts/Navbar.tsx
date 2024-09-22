d4ef36da012fb4b8c16bf35ae1b63d017083f4b2
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-22 22:36:18 +0300

    Refactor import paths in Navbar component and fix border style

--- client/src/layouts/Navbar.tsx
+++ client/src/layouts/Navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar() {
      */
     return (
         <>
-            <nav className="container-fluid h-16  border-b flex justify-between items-center">
+            <nav className="container-fluid h-16 border-b flex justify-between items-center">
                 <div className="logo ms-20">
                     <h1>LOGO</h1>
                 </div>
