1176e3d53d2d80cb157f96e954e2126e0fb6ec94
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-22 23:40:18 +0300

    Refactor import paths in Navbar component and update navigation links

--- client/src/layouts/Navbar.tsx
+++ client/src/layouts/Navbar.tsx
@@ -1,6 +1,5 @@
-import { Button } from "@/core/components/ui/button"
+import { Link } from "react-router-dom"
 import RotateTextButton from "./ui/RotateTextButton"
-
 
 export default function Navbar() {
     /**
@@ -20,16 +19,15 @@ export default function Navbar() {
         <>
             <nav className="container-fluid h-16 border-b flex justify-between items-center">
                 <div className="logo ms-20">
-                     {/* <h1>LOGO</h1>  */}
-                     <RotateTextButton text="Hover me" />
-                     <Button>Hover me</Button>
+                    <h1>LOGO</h1>
+
                 </div>
 
                 <div className="navbar ms-40">
-                    <ul className="flex gap-7">
-                        <li><a href="/">Accueil</a></li>
-                        <li><a href="/about">A propos</a></li>
-                        <li><a href="/contact">Contact</a></li>
+                    <ul className="flex gap-4">
+                        <li><Link to="/"><RotateTextButton text="Accueil" className="border-none" /></Link></li>
+                        <li><Link to="/about"><RotateTextButton text="A propos" className="border-none" /></Link></li>
+                        <li><Link to="/contact"><RotateTextButton text="Contact" className="border-none" /></Link></li>
                     </ul>
                 </div>
 
