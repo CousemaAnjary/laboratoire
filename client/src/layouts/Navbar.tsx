1ac06ea53d73e0bdddfdebc97a84aa7aeff89745
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-22 22:07:16 +0300

    Refactor import paths in Navbar component and update navigation links

--- client/src/layouts/Navbar.tsx
+++ client/src/layouts/Navbar.tsx
@@ -1,6 +1,4 @@
 
-
-
 export default function Navbar() {
     /**
      * ! STATE (état, données) de l'application
@@ -10,15 +8,25 @@ export default function Navbar() {
     /**
      * ! COMPORTEMENT (méthodes, fonctions) de l'application
      */
-   
+
 
     /**
      * ! AFFICHAGE (render) de l'application
      */
     return (
         <>
-            <nav className="container-fluid">
-               
+            <nav className="container-fluid p-2 border-b flex justify-between items-center">
+                <div className="logo ms-20">
+                    <h1>LOGO</h1>
+                </div>
+
+                <div className="navbar me-20">
+                    <ul className="flex gap-5">
+                        <li><a href="/">Accueil</a></li>
+                        <li><a href="/register">Inscription</a></li>
+                        <li><a href="/login">Connexion</a></li>
+                    </ul>
+                </div>
             </nav>
         </>
     )
