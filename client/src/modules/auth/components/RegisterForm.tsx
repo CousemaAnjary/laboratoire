c98c200ae90b8362e32a8232c1b23192d68cdcc2
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-23 10:45:36 +0300

    Refactor import paths in Navbar component and add RegisterForm component to Register page

--- client/src/modules/auth/components/RegisterForm.tsx
+++ client/src/modules/auth/components/RegisterForm.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/core/components/ui/button";
+import { Link } from "react-router-dom";
+
 export default function RegisterForm() {
     /**
      * ! STATE (état, données) de l'application
@@ -14,7 +17,33 @@ export default function RegisterForm() {
      */
     return (
         <>
-           
+            <div className="p-8 border bg-white">
+                <h1 className="scroll-m-20 text-3xl font-semibold tracking-tight mb-4 ">Créer un compte</h1>
+                <p className="mb-4 text-sm text-muted-foreground">
+                    Vous avez déjà un compte ? Accédez-y en cliquant <Link to="/login" className="underline text-cyan-700">ici</Link>
+                </p>
+
+                <form className="flex flex-col gap-4">
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="email" className="text-sm">Adresse email</label>
+                        <input type="email" id="email" name="email" className="input" placeholder="Votre adresse email" />
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="password" className="text-sm">Mot de passe</label>
+                        <input type="password" id="password" name="password" className="input" placeholder="Votre mot de passe" />
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <label htmlFor="confirmPassword" className="text-sm">Confirmer le mot de passe</label>
+                        <input type="password" id="confirmPassword" name="confirmPassword" className="input" placeholder="Confirmer votre mot de passe" />
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <Button type="submit" className="btn btn-primary">Créer un compte</Button>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
