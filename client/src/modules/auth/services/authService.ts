aae8b333e4d53c53505c7b252b1013fa5d277079
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-24 00:00:41 +0300

    Refactor import paths in Navbar component and update RegisterForm component in Register page

--- client/src/modules/auth/services/authService.ts
+++ client/src/modules/auth/services/authService.ts
@@ -1,5 +1,5 @@
 import api from "@/core/services/apiConfig"
-import { RegisterResponseType, RegisterType } from "../typeScript/AuthTypes"
+import { LoginResponseType, LoginType, RegisterResponseType, RegisterType } from "../typeScript/AuthTypes"
 
 // Inscription d'un utilisateur
 export const register = async (dataRegister: RegisterType): Promise<RegisterResponseType> => {
@@ -8,13 +8,7 @@ export const register = async (dataRegister: RegisterType): Promise<RegisterResp
 }
 
 // Connexion d'un utilisateur
-export const login = async () => {
-    try {
-        // Appel à l'API pour authentifier un utilisateur
-        const response = await api.post('/login')
-        return response.data // Retourner les données de la réponse de l'API
-
-    } catch (error) {
-        console.error('Erreur lors de l\'authentification:', error)
-    }
+export const login = async (dataLogin: LoginType): Promise<LoginResponseType> => {
+    const response = await api.post('/login', dataLogin)
+    return response.data // Retourner les données de la réponse de l'API
 }
