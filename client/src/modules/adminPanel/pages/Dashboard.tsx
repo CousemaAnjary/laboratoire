d756f0cbe6ab7779867fa810d7850a9fc597ad72
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-24 22:52:06 +0300

    Refactoriser la page de tableau de bord pour ajouter un bouton de déconnexion

--- client/src/modules/adminPanel/pages/Dashboard.tsx
+++ client/src/modules/adminPanel/pages/Dashboard.tsx
@@ -1,12 +1,30 @@
+import { Button } from "@/core/components/ui/button";
+import { useAuth } from "@/core/contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+
 export default function Dashboard() {
     /**
      * ! STATE (état, données) de l'application
      */
-
+    const navigate = useNavigate()
+    const { user, logout } = useAuth()
 
     /**
      * ! COMPORTEMENT (méthodes, fonctions) de l'application
      */
+    const handleLogout = async (): Promise<void> => {
+
+        try {
+            await logout()
+            // Déconnexion réussie, rediriger vers la page de connexion
+            navigate('/login')
+
+        } catch (error) {
+            // Afficher l'erreur dans la console
+            console.error('Logout failed', error)
+        }
+    }
 
 
     /**
@@ -14,9 +32,12 @@ export default function Dashboard() {
      */
     return (
         <>
-            <h1>
-                Dashboard Admin
-            </h1>
+            <h1>Dashboard Admin</h1>
+            <p className="text-sm font-medium leading-none mb-1"> {user?.last_name} {user?.first_name}</p>
+
+            <Button onClick={handleLogout}>
+                Se déconnecter
+            </Button>
         </>
     )
 }
