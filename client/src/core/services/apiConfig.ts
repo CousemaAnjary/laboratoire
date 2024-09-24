cab6bd413ec39f29dec21aaccf100093dea321a7
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-24 23:56:44 +0300

    Refactoriser les fichiers de configuration CORS et apiConfig.ts

--- client/src/core/services/apiConfig.ts
+++ client/src/core/services/apiConfig.ts
@@ -15,18 +15,22 @@ api.interceptors.request.use((config) => {
     // Récupérer le token JWT de localStorage
     const token = localStorage.getItem('token')
 
-    // Ajouter les headers par défaut
-    config.headers = config.headers || {}
-
     // Ajouter le token JWT dans les headers de la requête
     if (token) {
         config.headers['Authorization'] = `Bearer ${token}`
     }
 
+    // Ajouter les headers par défaut
+    config.headers = config.headers || {}
+
     // Ajouter le Content-Type pour les requêtes POST avec des données JSON
     config.headers['Content-Type'] = `multipart/form-data`
 
     return config
+
+}, (error) => {
+    return Promise.reject(error)
+
 })
 
 export default api
