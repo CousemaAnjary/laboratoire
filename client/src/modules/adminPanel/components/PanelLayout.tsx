debc87e34e65c7a61327aa111169cf73fb4dff46
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-25 21:06:32 +0300

    Refactoriser les composants Navbar, PanelLayout et Sidebar dans le module adminPanel

---
+++ client/src/modules/adminPanel/components/PanelLayout.tsx
@@ -0,0 +1,24 @@
+export default function PanelLayout() {
+    /**
+     * ! STATE (état, données) de l'application
+     */
+
+
+    /**
+     * ! COMPORTEMENT (méthodes, fonctions) de l'application
+     */
+
+
+    /**
+     * ! AFFICHAGE (render) de l'application
+     */
+    return (
+        <>
+            {/* Sidebar */}
+
+            {/* Conteneur principal */}
+
+            {/* Main content */}
+        </>
+    )
+}
