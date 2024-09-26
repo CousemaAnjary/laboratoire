6f5802c82fcb5f73a557edb60681e02ab32291bf
CousemaAnjary <anjaryabdillah@gmail.com>
2024-09-26 21:16:03 +0300

    Refactoriser la gestion des erreurs dans les formulaires de connexion et d'inscription

--- api/app/Http/Requests/Api/Auth/AuthenticatedUserSessionRequest.php
+++ api/app/Http/Requests/Api/Auth/AuthenticatedUserSessionRequest.php
@@ -26,4 +26,5 @@ class AuthenticatedUserSessionRequest extends FormRequest
             'password' => ['required', 'string'],
         ];
     }
+
 }
--- client/src/core/utils/handleApiError.ts
+++ client/src/core/utils/handleApiError.ts
@@ -23,6 +23,6 @@ export const handleApiError = (error: unknown): void => {
         }
     } else {
         // Erreur générique pour les erreurs inattendues
-        toast.error("Une erreur inattendue est survenue")
+        toast.error("Une erreur inattendue est survenue, Veuillez réessayer plus tard")
     }
 }
