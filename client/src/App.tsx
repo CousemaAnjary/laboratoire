453468ea83724b44b89aeb69de86739e6aa373fa
CousemaAnjary <anjaryabdillah@gmail.com>
2024-10-01 21:21:42 +0300

    Refactoriser la gestion de l'URL de l'API backend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ import { AuthProvider } from "./core/contexts/AuthContext"
 import Dashboard from "./modules/adminPanel/pages/Dashboard"
 
 
+
 export default function App() {
   /**
    * ! STATE (état, données) de l'application
