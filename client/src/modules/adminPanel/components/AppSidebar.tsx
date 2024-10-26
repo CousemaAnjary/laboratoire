1e77ab4033de463676b55403d56d31b4ee176832
CousemaAnjary <anjaryabdillah@gmail.com>
2024-10-26 17:33:27 +0300

    Refactoriser la gestion des composants UI dans Sidebar.tsx et AppSidebar.tsx

---
+++ client/src/modules/adminPanel/components/AppSidebar.tsx
@@ -0,0 +1,29 @@
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader } from "@/components/ui/sidebar";
+
+export default function AppSidebar() {
+    /**
+     * ! STATE (état, données) de l'application
+     */
+
+
+    /**
+     * ! COMPORTEMENT (méthodes, fonctions) de l'application
+     */
+
+
+    /**
+     * ! AFFICHAGE (render) de l'application
+     */
+    return (
+        <>
+            <Sidebar>
+                <SidebarHeader />
+                <SidebarContent>
+                    <SidebarGroup />
+                    <SidebarGroup />
+                </SidebarContent>
+                <SidebarFooter />
+            </Sidebar>
+        </>
+    )
+}
--- client/src/modules/adminPanel/components/Sidebar.tsx
+++ client/src/modules/adminPanel/components/Sidebar.tsx
@@ -1,19 +1,28 @@
-export default function Sidebar() {
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import AppSidebar from "./AppSidebar"
+
+
+export default function Sidebar({ children }: { children: React.ReactNode }) {
     /**
      * ! STATE (état, données) de l'application
      */
-    
-    
+
+
     /**
      * ! COMPORTEMENT (méthodes, fonctions) de l'application
      */
-    
-    
+
+
     /**
      * ! AFFICHAGE (render) de l'application
      */
     return (
-        <>
-        </>
+        <SidebarProvider>
+            <AppSidebar />
+            <main>
+                <SidebarTrigger />
+                {children}
+            </main>
+        </SidebarProvider>
     )
 }
