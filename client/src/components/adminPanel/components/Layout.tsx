07fb2db0fb761b9eb0df18e5a29cd0e60dfc857f
CousemaAnjary <anjaryabdillah@gmail.com>
2024-10-29 21:20:44 +0300

    Refactoriser l'import des composants UI dans App.tsx : Supprimer une ligne vide inutile

--- client/src/components/adminPanel/components/Layout.tsx
+++ client/src/components/adminPanel/components/Layout.tsx
@@ -1,24 +1,35 @@
 import Navbar from "./Navbar"
+import { useEffect, useState } from "react"
 import AppSidebar from "./AppSidebar"
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
 
+
+// Clé pour le localStorage
+const SIDEBAR_STATE_KEY = "sidebarState"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     /**
      * ! STATE (état, données) de l'application
      */
-
+    // Initialiser l'état `open` en récupérant la valeur depuis `localStorage`
+    const [open, setOpen] = useState(() => {
+        const savedState = localStorage.getItem(SIDEBAR_STATE_KEY);
+        return savedState ? JSON.parse(savedState) : false; // Par défaut, fermé
+    });
     /**
      * ! COMPORTEMENT (méthodes, fonctions) de l'application
      */
-
+    // Sauvegarder l'état dans `localStorage` à chaque changement
+    useEffect(() => {
+        localStorage.setItem(SIDEBAR_STATE_KEY, JSON.stringify(open));
+    }, [open]);
 
     /**
      * ! AFFICHAGE (render) de l'application
      */
     return (
         <>
-            <SidebarProvider>
+            <SidebarProvider open={open} onOpenChange={setOpen}>
                 <AppSidebar />
                 <SidebarInset>
                     <Navbar />
