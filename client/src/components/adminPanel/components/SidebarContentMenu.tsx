c0c03389e57408a10aa1f2e9cd058806ad57bb97
CousemaAnjary <anjaryabdillah@gmail.com>
2024-10-30 01:45:09 +0300

    Refactoriser l'import des composants UI dans App.tsx : Mettre à jour les liens de navigation pour les miettes de pain dynamiques

--- client/src/components/adminPanel/components/SidebarContentMenu.tsx
+++ client/src/components/adminPanel/components/SidebarContentMenu.tsx
@@ -31,22 +31,30 @@ export function SidebarContentMenu({ menuGroups }: SidebarContentMenuProps) {
                     {/* Menu du groupe */}
                     <SidebarMenu>
                         {group.menus.map((menu, index) => (
-                            <Collapsible key={index} className="group/collapsible" defaultOpen={menu.active ?? false}>
+                            <Collapsible
+                                key={index}
+                                defaultOpen={menu.active}
+                                className="group/collapsible"
+                                asChild
+                            >
 
                                 {/* Élément du menu */}
                                 <SidebarMenuItem>
                                     <CollapsibleTrigger asChild>
-                                        <SidebarMenuButton variant={menu.active ? "outline" : "default"} asChild>
-                                            <Link to={menu.href} >
-                                                <menu.icon className="text-black" />
+                                        <Link to={menu.href} >
+                                            <SidebarMenuButton
+                                                variant={menu.active ? "outline" : "default"}
+                                                tooltip={menu.label}
+                                            >
+                                                {menu.icon && <menu.icon className="text-black" />}
                                                 <span className="font-medium text-sm">{menu.label}</span>
 
                                                 {/* Affichage conditionnel de l'icône Chevron */}
                                                 {menu.submenus && menu.submenus.length > 0 && (
                                                     <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
                                                 )}
-                                            </Link>
-                                        </SidebarMenuButton>
+                                            </SidebarMenuButton>
+                                        </Link>
                                     </CollapsibleTrigger>
 
                                     {/* Sous-menu du menu (si disponibles) */}
@@ -57,11 +65,15 @@ export function SidebarContentMenu({ menuGroups }: SidebarContentMenuProps) {
                                                     <SidebarMenuSubItem key={index}>
 
                                                         {/* Bouton du sous-menu */}
-                                                        <SidebarMenuSubButton isActive={submenu.active ?? false} className="ms-1" asChild>
-                                                            <Link to={submenu.href}>
-                                                                <span className={`font-medium text-xs ${submenu.active ? "text-blue-900" : ""}`}>{submenu.label}</span>
-                                                            </Link>
-                                                        </SidebarMenuSubButton>
+                                                        <Link to={submenu.href}>
+                                                            <SidebarMenuSubButton
+                                                                isActive={submenu.active}
+                                                                className="ms-1"
+                                                                asChild
+                                                            >
+                                                                <span className={`font-medium text-xs ${submenu.active ? "text-blue-900 hover:text-blue-900" : ""}`}>{submenu.label}</span>
+                                                            </SidebarMenuSubButton>
+                                                        </Link>
                                                     </SidebarMenuSubItem>
                                                 ))}
                                             </SidebarMenuSub>
