8b30f9ed0d6d99975004e0f87e2aab781cc08c38
CousemaAnjary <anjaryabdillah@gmail.com>
2024-11-01 22:23:37 +0300

    Ajouter le composant Table et intégrer @tanstack/react-table, mettre à jour la page Utilisateurs pour améliorer l'affichage

--- client/src/modules/utilisateurs/pages/Utilisateurs.tsx
+++ client/src/modules/utilisateurs/pages/Utilisateurs.tsx
@@ -18,8 +18,11 @@ export default function Utilisateurs() {
     return (
         <>
             <Layout>
-                <div className="flex justify-between items-center mb-4">
+                <div className="flex justify-between items-center mb-8">
                     <h1 className="text-2xl">Gestion des utilisateurs</h1>
+                </div>
+
+                <div className="p-6 bg-muted/50 rounded-lg">
                 </div>
             </Layout>
         </>
