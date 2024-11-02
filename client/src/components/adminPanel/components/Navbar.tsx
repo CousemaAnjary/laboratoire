94676ef39efb541d938c7aa0d37c8a46317edea9
CousemaAnjary <anjaryabdillah@gmail.com>
2024-11-02 21:36:27 +0300

    Réorganiser le code du composant Navbar pour améliorer la lisibilité et la structure

--- client/src/components/adminPanel/components/Navbar.tsx
+++ client/src/components/adminPanel/components/Navbar.tsx
@@ -21,16 +21,11 @@ export default function Navbar() {
     return (
         <>
             <nav className="container-fluid sticky top-0 bg-white h-16 flex justify-between items-center">
-              
-
-                    <div className="flex items-center gap-2 px-4">
-                        <SidebarTrigger className="-ml-1" />
-                        <Separator orientation="vertical" className="mr-2 h-4" />
-                        <DynamicBreadcrumb />
-                    </div>
-
-             
-
+                <div className="flex items-center gap-2 px-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
+                    <DynamicBreadcrumb />
+                </div>
                 <div className="flex justify-end space-x-3 me-10">
                     <UserDropdownMenu />
                 </div>
