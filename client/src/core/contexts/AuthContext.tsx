d37f7d5bb63b6cc1ffefb65a2c6226d8ca30c2a7
CousemaAnjary <anjaryabdillah@gmail.com>
2024-11-06 00:26:47 +0300

    Réorganiser les imports dans AuthContext pour utiliser les nouveaux chemins de service et de types d'authentification

--- client/src/core/contexts/AuthContext.tsx
+++ client/src/core/contexts/AuthContext.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable react-refresh/only-export-components */
 import { isAuthenticated } from "../utils/auth"
 import { createContext, ReactNode, useContext, useEffect, useState } from "react"
-import { login as loginService, logout as logoutService } from '@/modules/authentifications/services/authService'
-import { AuthContextType, LoginResponseType, LoginType, UserType } from "@/modules/authentifications/typeScript/AuthTypes"
+import { login as loginService, logout as logoutService } from '@/modules/authentifications/login/loginService'
+import { AuthContextType, LoginResponseType, LoginType, UserType } from "@/modules/authentifications/login/loginType"
 
 
 /**
