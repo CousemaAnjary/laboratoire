53eec1852e1cc7943cecf83d8d39277c056c3453
CousemaAnjary <anjaryabdillah@gmail.com>
2024-11-10 20:46:53 +0300

    Refactor le composant DataTable pour améliorer la structure du code et ajouter un fichier de type TypeScript pour les props.

--- client/src/components/data-table/components/DataTable.tsx
+++ client/src/components/data-table/components/DataTable.tsx
@@ -1,44 +1,40 @@
-"use client";
+import { DataTableProps } from "../typeScript/dataTableType"
+import { flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 
-import {
-    ColumnDef,
-    flexRender,
-    getCoreRowModel,
-    useReactTable,
-} from "@tanstack/react-table";
 
-import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table";
 
-interface DataTableProps<TData, TValue> {
-    columns: ColumnDef<TData, TValue>[];
-    data: TData[];
-}
 
-export function DataTable<TData, TValue>({
-    columns,
-    data,
-}: DataTableProps<TData, TValue>) {
-    const table = useReactTable({
-        data,
-        columns,
-        getCoreRowModel: getCoreRowModel(),
-    });
+export default function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData, TValue>) {
+    /**
+     * ! STATE (état, données) de l'application
+     */
 
+    // Utilisation du hook useReactTable pour gérer les données du tableau
+    const table = useReactTable({ data, columns, getCoreRowModel: getCoreRowModel() })
+
+
+    /**
+     * ! COMPORTEMENT (méthodes, fonctions) de l'application
+     */
+
+
+    /**
+     * ! AFFICHAGE (render) de l'application
+     */
     return (
         <div className="rounded-md border">
             <Table>
                 <TableHeader>
+
+                    {/* Affichage des en-têtes de colonnes */}
                     {table.getHeaderGroups().map((headerGroup) => (
                         <TableRow key={headerGroup.id}>
+
+
                             {headerGroup.headers.map((header) => (
                                 <TableHead key={header.id}>
+
                                     {header.isPlaceholder
                                         ? null
                                         : flexRender(
@@ -50,7 +46,10 @@ export function DataTable<TData, TValue>({
                         </TableRow>
                     ))}
                 </TableHeader>
+
                 <TableBody>
+
+                    {/* Affichage des lignes de données */}
                     {table.getRowModel().rows.length ? (
                         table.getRowModel().rows.map((row) => (
                             <TableRow key={row.id}>
@@ -61,14 +60,18 @@ export function DataTable<TData, TValue>({
                                 ))}
                             </TableRow>
                         ))
-                    ) : (
+
+                    ) : (  
                         <TableRow>
+                            
+                            {/* Aucun résultat */}
                             <TableCell colSpan={columns.length} className="h-24 text-center">
                                 Aucun résultat.
                             </TableCell>
                         </TableRow>
                     )}
                 </TableBody>
+
             </Table>
         </div>
     );
