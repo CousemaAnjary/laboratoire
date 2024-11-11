edc05de5ff20818779cad79b1cfca600fe28ac95
CousemaAnjary <anjaryabdillah@gmail.com>
2024-11-11 20:51:40 +0300

    Ajouter le composant Checkbox et refactoriser les colonnes du DataTable pour inclure la sélection des lignes

--- client/src/components/data-table/typeScript/dataTableType.ts
+++ client/src/components/data-table/typeScript/dataTableType.ts
@@ -1,6 +1,13 @@
-import { ColumnDef } from "@tanstack/react-table"
+import { Column, ColumnDef } from "@tanstack/react-table"
 
 export interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[];
     data: TData[];
+}
+
+
+export interface DataTableColumnHeaderProps<TData, TValue>
+  extends React.HTMLAttributes<HTMLDivElement> {
+  column: Column<TData, TValue>;
+  title: string;
 }
---
+++ client/src/modules/parametres/utilisateurs/utils/columns.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { Payment } from "./data";
+import { ColumnDef } from "@tanstack/react-table";
+import { DataTableColumnHeader } from "@/components/data-table/components/DataTableColumnHeader";
+import { Checkbox } from "@/components/ui/checkbox";
+
+// Définition des colonnes en utilisant ColumnDef de @tanstack/react-table
+export const columns: ColumnDef<Payment>[] = [
+    {
+        id: "select",
+        header: ({ table }) => (
+            <Checkbox
+                checked={
+                    table.getIsAllPageRowsSelected() ||
+                    (table.getIsSomePageRowsSelected() && "indeterminate")
+                }
+                onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+                aria-label="Select all"
+                className="translate-y-0.5 shadow-none"
+            />
+        ),
+        cell: ({ row }) => (
+            <Checkbox
+                checked={row.getIsSelected()}
+                onCheckedChange={(value) => row.toggleSelected(!!value)}
+                aria-label="Select row"
+                className="translate-y-0.5 shadow-none"
+            />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+    },
+    {
+        accessorKey: "nom",
+        header: ({ column }) => (
+            // Assurez-vous que cette syntaxe JSX est correcte
+            <DataTableColumnHeader column={column} title="Nom" />
+        ),
+    },
+    {
+        accessorKey: "prenom",
+        header: "Prénom"
+    },
+    {
+        accessorKey: "email",
+        header: "Email"
+    },
+    {
+        accessorKey: "statut",
+        header: "Statut"
+    },
+    {
+        accessorKey: "rôle",
+        header: "Rôle"
+    },
+];
+
