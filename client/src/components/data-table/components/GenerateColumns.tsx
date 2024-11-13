3ef114674ea5b1216ddb78661fe00871d1e2aa05
CousemaAnjary <anjaryabdillah@gmail.com>
2024-11-13 12:13:03 +0300

    Mettre à jour la fonction GenerateColumns pour utiliser un composant d'en-tête personnalisé

--- client/src/components/data-table/components/GenerateColumns.tsx
+++ client/src/components/data-table/components/GenerateColumns.tsx
@@ -1,9 +1,12 @@
 import { ColumnDef } from "@tanstack/react-table";
+import { DataTableColumnHeader } from "./DataTableColumnHeader";
 
 // Fonction générique pour générer automatiquement les colonnes avec des en-têtes en majuscules
 export function GenerateColumns<T>(keys: Array<keyof T>): ColumnDef<T>[] {
     return keys.map((key) => ({
         accessorKey: key as string,
-        header: String(key).toUpperCase(),
+        header: ({ column }) => (
+            <DataTableColumnHeader column={column} title={String(key).toUpperCase()} />
+        ),
     })) as ColumnDef<T>[];
 }
