06699bf1099dabca60efbe73916c7c95ca126b97
CousemaAnjary <anjaryabdillah@gmail.com>
2024-11-13 12:27:04 +0300

    Mettre à jour le type Utilisateur et ajouter un nouvel exemple de données

--- client/src/modules/parametres/utilisateurs/utils/data.ts
+++ client/src/modules/parametres/utilisateurs/utils/data.ts
@@ -1,5 +1,5 @@
 // data.ts
-export type Utilisateur = {  
+export type Utilisateur = {
     nom: string;
     prénom: string;
     email: string;
@@ -8,17 +8,24 @@ export type Utilisateur = {
 
 // Exemple de données
 export const utilisateurData: Utilisateur[] = [
-    {   
+    {
         nom: "John",
         prénom: "Doe",
         email: "john.doe@example.com",
-        statut: "Actif",  
+        statut: "Actif",
     },
     {
         nom: "ABDILLAH",
         prénom: "Cousema",
         email: "cousema@example.com",
         statut: "Inactif",
+    },
+
+    {
+        nom: "BEN MOHAMED",
+        prénom: "Nour",
+        email: "nour@gmail.com",
+        statut: "Actif",
     },
 ];
 
