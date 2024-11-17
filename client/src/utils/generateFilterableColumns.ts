f211ce4ea0d33c557c2349d718f302cf18c66fc1
CousemaAnjary <anjaryabdillah@gmail.com>
2024-11-17 18:18:18 +0300

    Améliorer la fonction generateFilterableColumns en normalisant la capitalisation des étiquettes des options de filtrage et en simplifiant le code pour la vérification des types.

--- client/src/utils/generateFilterableColumns.ts
+++ client/src/utils/generateFilterableColumns.ts
@@ -8,15 +8,13 @@ export function generateFilterableColumns<T>(
         const uniqueValues = Array.from(new Set(data.map((item) => item[key])));
 
         // Vérifiez si la clé est de type chaîne avant d'utiliser `charAt` et `slice`
-        const title = typeof key === 'string'
-            ? key.charAt(0).toUpperCase() + key.slice(1)
-            : String(key);
+        const title = typeof key === 'string' ? key.charAt(0).toUpperCase() + key.slice(1) : String(key);
 
         return {
             id: key,
             title, // Utiliser le titre après vérification du type
             options: uniqueValues.map((value) => ({
-                label: String(value), // Convertir les valeurs en chaînes
+                label: String(value).charAt(0).toUpperCase() + String(value).slice(1),
                 value: String(value),
             })),
         };
