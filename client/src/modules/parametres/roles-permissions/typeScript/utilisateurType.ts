b89d7becd2dbe72949169ff80e32077f7da3b3a7
CousemaAnjary <anjaryabdillah@gmail.com>
2024-12-28 17:28:09 +0300

    Mettre à jour l'interface des rôles pour utiliser des objets Permission et ajuster la fonction dataRole pour extraire les noms des permissions.

--- client/src/modules/parametres/roles-permissions/typeScript/utilisateurType.ts
+++ client/src/modules/parametres/roles-permissions/typeScript/utilisateurType.ts
@@ -4,11 +4,18 @@ export interface UtilisateurRowActionsProps<TData> {
     row: Row<TData>
 }
 
+export interface Permission {
+    id: number;
+    name: string;
+}
+
+
 export interface Roles {
     name: string; // Nom du Profil
     description: string; // Description du profil
-    permissions: string[]; // Liste des permissions (Lire, Écrire, Modifier, etc.)
+    permissions: Permission[]; // Liste des permissions (Lire, Écrire, Modifier, etc.)
     statut: string; // Statut (actif ou inactif)
     derniereMiseAJour: string; // Dernière Mise à Jour
     utilisateursAssignes: number; // Nombre d'utilisateurs assignés
 }
+
--- client/src/modules/parametres/roles-permissions/utils/data.ts
+++ client/src/modules/parametres/roles-permissions/utils/data.ts
@@ -4,7 +4,7 @@ export function dataRole(roles: Roles[]): Roles[] {
     return roles.map((role) => ({
         name: role.name || "Non spécifié",
         description: role.description || "Pas de description",
-        permissions: role.permissions || ["lire", "écrire", "modifier"],
+        permissions: role.permissions.map((permission) => permission.name) || [],
         statut: role.statut || "inconnu",
         derniereMiseAJour: role.derniereMiseAJour || "Non spécifié",
         utilisateursAssignes: role.utilisateursAssignes || 0,
